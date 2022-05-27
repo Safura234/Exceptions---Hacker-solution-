d4c92374c58bb566cbd8a25d474e18e4370fd0fb
Safura234 <safuramg@code.edu.az>
2022-05-27 16:51:10 +0400

    https://github.com/Safura234/Exceptions---Hacker-solution-.git

---
+++ Day 16 exceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day_16_exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string S = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(int.Parse(S));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
